24e312c4a10f80c9d78d5c758eb48ecfcd4d438c
yarhamjohn <yarhamjohn@gmail.com>
2021-02-05 16:56:34 +0000

    Add empty test project

---
+++ blazor-games.tests/Minesweeper/MinesweeperTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace blazor_games.tests.Minesweeper
+{
+    public class MinesweeperTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
